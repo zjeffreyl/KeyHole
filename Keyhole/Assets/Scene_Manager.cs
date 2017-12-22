a14dd47824ee0b0a20dadbb8181d2610903ecdf9
Mebin Skaria <mebins@gmail.com>
2017-12-22 15:36:07 -0800

    Added Main Menu Scene and a Scene Manager Script/ Game Object

---
+++ Keyhole/Assets/Scene_Manager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene_Manager : MonoBehaviour
+{
+
+    public void jump_to(string level_name)
+    {
+        Application.LoadLevel(level_name);
+    }
+
+}
