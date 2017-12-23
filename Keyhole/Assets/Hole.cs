2ef0ef4b0efeddd56a940b298bcae94ebeae400f
Mebin Skaria <mebins@gmail.com>
2017-12-22 16:48:44 -0800

    Createad a Key, Hole and got both the rotation scripts working.

---
+++ Keyhole/Assets/Hole.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour {
+    public float rotation_speed;
+    private Vector3 v_rotation;
+	// Use this for initialization
+	void Start () {
+      
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(v_rotation);
+    }
+
+    void FixedUpdate()
+    {
+        v_rotation = new Vector3(0, 0, -rotation_speed);
+    }
+  
+}
---
+++ Keyhole/Assets/Key.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+    public float orbit_speed; 
+    private Vector3 v_rotation;
+    public Hole hole;
+    public float distance;
+    // Use this for initialization
+    private Vector3 zAxis = new Vector3(0, 0, 1);
+	void Start () {
+        this.transform.position += new Vector3(0,distance,10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(Vector3.zero, zAxis , orbit_speed * Time.deltaTime);
+	}
+}
