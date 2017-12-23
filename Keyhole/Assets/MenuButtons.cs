9797ee0fa817555a8adcc2873e0c19e6fb11f0a2
DESKTOP-46NOMGO\Jeffrey Zhang <zjeffreyl@gmail.com>
2017-12-22 16:50:43 -0800

    Basic Start Screen

---
+++ Keyhole/Assets/MenuButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void BeginGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
