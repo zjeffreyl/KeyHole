a1ebcc6720592e16cc54635d60597c46b86e860e
zjeffreyl <zjeffreyl@gmail.com>
2017-12-31 10:17:04 -0800

    States implemented to control

--- Keyhole/Assets/Key.cs
+++ Keyhole/Assets/Key.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Key : MonoBehaviour {
+public class Key : MonoBehaviour
+{
+    public enum LevelState { PLAYING, RESULT, RESPAWN };
+    //State
+    public LevelState state = LevelState.PLAYING;
     //Victory particle
     public GameObject victoryParticle;
     public Transform particleLocation;
 
     public float threshold = 0.05f;
 
-    private bool checkOnce = false;
+    private bool pressBAllowed = true;
     private bool trigger = false;
     public float orbit_speed;
     public float slide_speed;
@@ -20,17 +24,19 @@ public class Key : MonoBehaviour {
     public float distance;
     // Use this for initialization
     private Vector3 zAxis = new Vector3(0, 0, 1);
-	void Start () {
-        this.transform.position += new Vector3(0,distance,10);
-	}
-    
+    void Start()
+    {
+        this.transform.position += new Vector3(0, distance, 10);
+    }
+
     public void checkWin()
     {
         float angleDiff = (Mathf.Abs(this.transform.rotation.z) - Mathf.Abs(hole.transform.rotation.z));
         if (Mathf.Abs(angleDiff) < threshold)
         {
-            Instantiate(victoryParticle, particleLocation.transform.position, particleLocation.transform.rotation);
-            //GameObject.Destroy(victoryParticle, 1f);
+            GameObject clone = (GameObject)Instantiate(victoryParticle, particleLocation.transform.position, particleLocation.transform.rotation);
+            Destroy(clone.gameObject, 1f);
+            
         }
         else
         {
@@ -38,38 +44,48 @@ public class Key : MonoBehaviour {
 
         }
     }
-    
+
     public void increaseDifficulty()
     {
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-        transform.RotateAround(Vector3.zero, zAxis , orbit_speed * Time.deltaTime);
-        //Check the angle here if so Instantiate
-        if (Input.GetKey(KeyCode.B) || trigger)
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == LevelState.PLAYING)
         {
-            orbit_speed = temp_speed;
-            orbit_speed = halt_speed;
-            trigger = true;
-            Debug.Log("Receiving the input");
-            float move = slide_speed * Time.deltaTime;
-            trigger = false;
-            transform.position = Vector3.MoveTowards(transform.position, hole.transform.position, move);
-            //check here
-            checkOnce = true;
+            pressBAllowed = true;
         }
-        //at 
-        if(this.transform.position == hole.transform.position && checkOnce == true)
+        transform.RotateAround(Vector3.zero, zAxis, orbit_speed * Time.deltaTime);
+        //Check the angle here if so Instantiate
+        if (Input.GetKey(KeyCode.B) && pressBAllowed == true)
         {
-            checkOnce = false;
-            //no more trigger and running move
-            trigger = false;
-            Debug.Log("Key is at the target");
-            hole.rotation_speed = temp_speed;
-            hole.rotation_speed = halt_speed;
-            checkWin();
+            //pressed once and the result will continue
+            state = LevelState.RESULT;
+            //While in 'PRESSED B' @ anytime cannot access again
+            if(state == LevelState.RESULT)
+            {
+                //lock input
+                pressBAllowed = false;
+                //now check result
+            }
+            orbit_speed = 0;
+            trigger = true;
+            while (trigger == true)
+            {
+                Debug.Log("Receiving the input in loop");
+                float move = slide_speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, hole.transform.position, move);
+                //at target position needs to stop here and exit loop
+                if (this.transform.position == hole.transform.position)
+                {
+                    Debug.Log("Key is at the target");
+                    checkWin();
+                    //exit loop
+                    trigger = false;
+                }
+            }
         }
     }
 }
